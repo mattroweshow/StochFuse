6ffe2ff0c8dee92d8f8d2b546e6a8bf20ea4677a
Matthew Rowe <rowem@scc-culture-mind.lancs.ac.uk>
2015-07-31 16:33:14 +0100

    changed mesos node

--- Spark/Spark-Data-Cleaner.py
+++ Spark/Spark-Data-Cleaner.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     # Added the core limit to avoid resource allocation overruns
     conf.set("spark.cores.max", "20")
 #    conf.setMaster("mesos://zk://scc-culture-mind.lancs.ac.uk:2181/mesos")
-    conf.setMaster("mesos://zk://scc-culture-mind.lancs.ac.uk:7077/mesos")
+    conf.setMaster("mesos://zk://scc-culture-slave4.lancs.ac.uk:2181/mesos")
     conf.set("spark.executor.uri", "hdfs://scc-culture-mind.lancs.ac.uk/lib/spark-1.3.0-bin-hadoop2.4.tgz")
     conf.set("spark.broadcast.factory", "org.apache.spark.broadcast.TorrentBroadcastFactory")
 
